c02315903bb75f9dd6ce34c05fa199e78ac5f07e
zumer305 <zumerniaz305@gmail.com>
2025-09-13 00:50:11 +0500

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello!");
+console.log("zumer");
+
+// operation
+let a=1;
+let b=1;
+console.log("sum is",a+b);
+
+
+// Template literals
+let x=10;
+let y=20;
+console.log("(console ,)The total price of x and y is:" , (x+y) , " Ruppes.");
+let z= "(zVariable +)The total price of x and y is:" + (x+y) + " Ruppes."
+console.log(z);
+
+// backticks
+console.log(`(Backticks escp)The total price of x and y is: ${x+y} Ruppes.`);
+
+
+
