db271b385a9af66c12ed8f5f2fd37a7909e95b43
zumer305 <zumerniaz305@gmail.com>
2025-09-13 22:30:57 +0500

    update

---
+++ test.js
@@ -0,0 +1,36 @@
+// q1
+let num=20;
+if(num%10==0)
+    console.log("Good");
+else
+    console.log("Bad");
+
+
+// q2
+// let n=prompt("Name");
+// let age=prompt("age");
+// let msj=`${n} is ${age} years old`;
+// alert(msj);
+
+// q3
+let quater=1;
+switch(quater){
+    case 1: 
+    console.log("jan,feb,mar");
+    break;
+    case 2:
+        console.log("ap,may,june");
+        break;
+        case 3:
+            console.log("july,august,sep");
+            break;
+
+            case 4:
+                console.log("oct,nov,dec");
+                break;
+
+                default:
+                    console.log("noo quater");
+}
+
+
