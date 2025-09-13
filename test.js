8af8714473b433a6b11a52b10ece751b7ddb3f88
zumer305 <zumerniaz305@gmail.com>
2025-09-14 01:19:30 +0500

    update

--- test.js
+++ test.js
@@ -34,3 +34,34 @@ switch(quater){
 }
 
 
+// q4
+let str="Applee";
+if(((str[0]=="A")||(str[0]=="a"))&&(str.length>5)){
+    console.log("Golden");
+}
+else{
+    console.log("Not Golden");
+}
+
+// q5
+let a=2;
+let b=3;
+let c=4;
+if(a>b){
+    if(a>c) console.log(a);
+    else console.log(c);
+}
+else{
+    if(b>c) console.log(b);
+    else console.log(c);
+}
+
+// q6--remainder
+let num1=1234;
+let num2=21;
+if((num1%10)===(num2%10)){
+    console.log("same");
+}
+else{
+    console.log("not same");
+}
